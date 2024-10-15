32afecfcefe02149a0c262acb54d2271df9aaf8d
Guilherme França <131270776+GuilFranca@users.noreply.github.com>
2024-10-15 11:47:07 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId);
+ 
+    toggle.addEventListener('click', () => {
+        // Adiciona a classe show-menu ao menu de navegação
+        nav.classList.toggle('show-menu');
+ 
+        // Adiciona a classe show-icon para mostrar e esconder o ícone do menu
+        toggle.classList.toggle('show-icon');
+    });
+};
+ 
+showMenu('nav-toggle', 'nav-menu');
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Função para alternar o dropdown
+    const toggleDropdown = (dropdown) => {
+        const dropdownMenu = dropdown.querySelector('.dropdown__menu');
+        const arrow = dropdown.querySelector('.dropdown__arrow');
+
+        if (dropdownMenu.style.maxHeight) {
+            dropdownMenu.style.maxHeight = null;
+            arrow.style.transform = 'rotate(0deg)';
+        } else {
+            dropdownMenu.style.maxHeight = dropdownMenu.scrollHeight + "px";
+            arrow.style.transform = 'rotate(180deg)';
+        }
+    };
+
+    // Função para alternar o submenu
+    const toggleSubmenu = (submenu) => {
+        const dropdownSubmenu = submenu.querySelector('.dropdown__submenu');
+        const addIcon = submenu.querySelector('.dropdown__add');
+
+        if (dropdownSubmenu.style.maxHeight) {
+            dropdownSubmenu.style.maxHeight = null;
+            addIcon.style.transform = 'rotate(0deg)';
+        } else {
+            dropdownSubmenu.style.maxHeight = dropdownSubmenu.scrollHeight + "px";
+            addIcon.style.transform = 'rotate(45deg)';
+        }
+    };
+
+    // Seleciona todos os dropdowns
+    const dropdowns = document.querySelectorAll('.dropdown__item');
+
+    dropdowns.forEach((dropdown) => {
+        const dropdownLink = dropdown.querySelector('.nav__link');
+        dropdownLink.addEventListener('click', () => {
+            toggleDropdown(dropdown);
+        });
+    });
+
+    // Seleciona todos os submenus
+    const submenus = document.querySelectorAll('.dropdown__subitem');
+
+    submenus.forEach((submenu) => {
+        const submenuLink = submenu.querySelector('.dropdown__link');
+        submenuLink.addEventListener('click', () => {
+            toggleSubmenu(submenu);
+        });
+    });
+});
